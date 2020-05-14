b36d98fb70df192657691bac446545199e205011
Rauf Rajput <r.rajput@hotmail.com>
2020-05-14 19:00:58 +0200

    edited about page

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,93 @@
 <template>
+
   <div class="about">
-    <h1>This is an about page</h1>
+
+    <div class="title">About us who developed this quiz for learning and education</div>
+
+    <div class="info">
+
+      <li>This is a school project that covers Webapplictions and Agile Methods.</li><br><br>
+
+
+      This project focuses on SCRUM, Vue.cli, node.js with an postgress database.<br><br>
+
+      Team-members (link to Github):
+
+<br>
+
+      <div><a href="https://github.com/lfgn84" target="_blank">Luis Guiteraez</a></div>
+
+      <div><a href="https://github.com/SebastianWaltilla" target="_blank">Sebastian Waltilla</a></div>
+
+      <div><a href="https://github.com/bigJohanna" target="_blank">Johanna Svallingson</a></div>
+
+      <div><a href="https://github.com/mansfritzner" target="_blank">Måns Fritzner</a></div>
+
+      <div><a href="https://github.com/RaufRajput" target="_blank">Rauf Rajput</a></div>
+
+
+    </div>
+
   </div>
+
 </template>
+
+
+
+
+
+<script>
+
+  export default {
+    name: "About",
+  }
+
+</script>
+
+
+<style scoped>
+
+  .about{
+
+    display:grid;
+
+    grid-template-columns: repeat(1, auto);
+
+    grid-template-rows: repeat(3, auto);
+
+
+    justify-content: center;
+
+    margin: 1em;
+
+    padding: 0px 15px 0px 15px
+
+  }
+
+
+
+  .title {
+
+    text-decoration: underline;
+
+    grid-row: 1;
+
+    grid-column: 1;
+
+
+
+  }
+
+
+
+  .info{
+
+    text-align: left;
+
+    margin-top: 1vmax;
+
+    white-space: pre-line;
+
+  }
+
+</style>
