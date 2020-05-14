56491a53e85deaba10d46191c91612721a872832
Luis <lfgn84@gmail.com>
2020-05-14 21:04:43 +0200

    created gameComponent

---
+++ frontend/src/components/gameComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="game">
+    <diV>GAME COMPONENT</diV>
+    <div>Choosen level: {{gameLevel}}</div>
+    <div>Choosen table: {{gameTable}}</div>
+    <div>{{mathTables}}</div>
+    </div>
+</template>
+
+<script>
+    import json from '../assets/json/mathTables.json'
+    export default {
+        name: "gameComponent",
+        props:{
+            table: Number,
+            level: Number,
+        },
+        data(){
+            return {
+                mathTables: json,
+                gameTable:"",
+                gameLevel:""
+            }
+        },
+        watch:{
+            table: function () {
+                this.gameTable = this.table
+            },
+            level: function(){
+                this.gameLevel = this.level
+            }
+        }
+    }
+</script>
+
+<style scoped>
+#game{
+    background-color: #2c3e50;
+    color: white;
+}
+</style>
--- frontend/src/views/singlePlay.vue
+++ frontend/src/views/singlePlay.vue
@@ -1,22 +1,49 @@
 <template>
-
+<div>
     <div class="header">
         <h1>Single Player</h1>
-
-    {{mathTables}}
-
     </div>
+    <div>Choose multpilication table : <input v-model="input.inputTable"/></div>
+    <div>Choose difficulty level : <input v-model="input.inputLevel"/></div>
+    <button @click="setConfig">OK</button>
+    <gameComponent :table="userTable" :level="userLevel" v-show="gameOn"></gameComponent>
+
+</div>
 </template>
 
 <script>
-    import json from '../assets/json/mathTables'
+import gameComponent from "../components/gameComponent";
     export default {
         name: "singlePlay",
+        components: {gameComponent},
         data(){
             return{
-                mathTables:json
+                input:{
+                    inputTable: "",
+                    inputLevel: ""
+                },
+                userLevel: "",
+                userTable: "",
+                gameOn: false
+
+
+
+
+            }
+        },
+        methods:{
+            setConfig: function(){
+                let tab = this.input.inputTable;
+                this.userTable = tab;
+                let lev = this.input.inputLevel;
+                this.userLevel = lev;
+
+                this.input.inputTable = "";
+                this.input.inputLevel = "";
+                this.gameOn = true
             }
         }
+
 }
 </script>
 
@@ -26,7 +53,6 @@
 
     .header {
         font-family: 'Luckiest Guy', Tahoma,serif;
-        position: fixed;
         width: 100%;
         color: #f8fff9;
         text-align: center;
