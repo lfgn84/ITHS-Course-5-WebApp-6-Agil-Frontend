b6e1029ae17eb92440eefa29ddb300f3604cbb51
Rauf Rajput <r.rajput@hotmail.com>
2020-05-14 17:51:46 +0200

    minor changes

--- frontend/src/views/singlePlay.vue
+++ frontend/src/views/singlePlay.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
+
+    <div class="header">
+        <h1>Single Player</h1>
+
     {{mathTables}}
+
     </div>
 </template>
 
@@ -17,5 +21,20 @@
 </script>
 
 <style scoped>
+
+    @import url('https://fonts.googleapis.com/css?family=Luckiest+Guy&display=swap');
+
+    .header {
+        font-family: 'Luckiest Guy', Tahoma,serif;
+        position: fixed;
+        width: 100%;
+        color: #f8fff9;
+        text-align: center;
+        background: #2e3af5;
+        font-size: 30px;
+        margin-left: -10px;
+    }
+
+
 
 </style>
