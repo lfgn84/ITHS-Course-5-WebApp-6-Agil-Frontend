aa188676193e7751b384b684750a6a044748b697
Johanna <svallingson@gmail.com>
2020-05-28 12:03:40 +0200

    winner vue

--- frontend/src/views/winner.vue
+++ frontend/src/views/winner.vue
@@ -1,12 +1,47 @@
 <template>
     <div>
-        <p> hej winner view</p>
+        <p> winner view</p>
 
-        <p>{{room}}</p>
-        <p>{{time}}</p>
-        <p>{{nickname}}</p>
-        <p>{{score}}</p>
+        <div>
+            <p>Your result:</p>
+            <p>Score: {{score}}</p>
+            <p>Total time: {{time}}</p>
+        </div>
 
+        <div v-if="loading" class="loading">
+            Loading results from all players...
+        </div>
+
+        <div>
+
+            <table style="width:50%">
+                <tr>
+                    <th>Ranking</th>
+                    <th>Nickname</th>
+                    <th>Score</th>
+                    <th>Time</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>{{playerArray[0].nickname}}</td>
+                    <td>{{playerArray[0].correctanswers}}</td>
+                    <td>{{playerArray[0].totaltime}}</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>{{playerArray[1].nickname}}</td>
+                    <td>{{playerArray[1].correctanswers}}</td>
+                    <td>{{playerArray[1].totaltime}}</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>{{playerArray[2].nickname}}</td>
+                    <td>{{playerArray[2].correctanswers}}</td>
+                    <td>{{playerArray[2].totaltime}}</td>
+                </tr>
+            </table>
+
+        </div>
 
 
     </div>
@@ -26,16 +61,44 @@
             score:0,
             nickname: "",
             room: "",
-            time: 0
+            time: 0,
+            loading : true,
+            playerArray: []
         }
     },
     created() {
+        //hämta värden från multiplay
         this.nickname = this.$route.params.nickname;
         this.room = this.$route.params.room;
         this.score = this.$route.params.score;
         this.time = this.$route.params.time;
 
     },
+        mounted() {
+          //hämta winnertable med room i requestbody
+
+            var url = new URL('https://fierce-mountain-27289.herokuapp.com/v1/winner')
+            var params = {room: this.room}
+            url.search = new URLSearchParams(params).toString()
+
+
+            fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            })
+                .then(response => response.json())
+                .then(data => {
+                    this.playerArray = data.results
+                    this.loading = false;
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+
+        },
+
         methods: {
             formatTotalTime(){
                 //räkna ut hur många minuter och sekunder timePassed är som i från början endast räknas i antal sekunder
