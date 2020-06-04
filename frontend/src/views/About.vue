e3345dc53ea6696f49fe0d74f5aff27c7f3a54dd
mans <mans.fritzner@gmail.com>
2020-06-04 09:11:50 +0200

    fixinf about page

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -2,35 +2,7 @@
 
 
   <div class="about">
-      <div id="nav">
-      <router-link to="/">Home</router-link>
-      </div>
 
-    <div class="title">About us who developed this quiz for learning and education</div>
-
-    <div class="info">
-
-      <li>This is a school project that covers Webapplictions and Agile Methods.</li><br><br>
-
-
-      This project focuses on SCRUM, Vue.cli, node.js with an postgress database.<br><br>
-
-      Team-members (link to Github):
-
-<br>
-
-      <div><a href="https://github.com/lfgn84" target="_blank">Luis Guiteraez</a></div>
-
-      <div><a href="https://github.com/SebastianWaltilla" target="_blank">Sebastian Waltilla</a></div>
-
-      <div><a href="https://github.com/bigJohanna" target="_blank">Johanna Svallingson</a></div>
-
-      <div><a href="https://github.com/mansfritzner" target="_blank">Måns Fritzner</a></div>
-
-      <div><a href="https://github.com/RaufRajput" target="_blank">Rauf Rajput</a></div>
-
-
-    </div>
 
   </div>
 
@@ -42,8 +14,13 @@
 
 <script >
 
+  import AboutComponent from "../components/aboutComponent";
+
   export default {
     name: "About",
+    components: {
+      AboutComponent,
+    }
   }
 
 </script>
